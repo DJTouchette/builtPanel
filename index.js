745f933c617613053a034027c332016d897b7e2a
djtouchette <djtouchette1993@gmail.com>
2016-07-15 01:45:48 -0700

    fixed

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('*', function(req, res) {
+app.get('*',function(req, res) {
 		const fullurl = req.hostname + req.originalUrl;
 		console.log(fullurl);
 
-    res.sendFile( __dirname + '/build/index.html');
+    res.sendFile( __dirname + '/index.html');
 });
+app.use(express.static(path.join(__dirname)));
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.listen(62102, () => {
+app.listen(8080, () => {
   console.log('listening 3000');
 });
