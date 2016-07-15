e674298e8d2ae431fe3a5ed0a7fbde05805d48d6
djtouchette <djtouchette1993@gmail.com>
2016-07-15 01:59:07 -0700

    static

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const adminPort = 62102;
+// const adminPort = 62102;
+const adminPort = 8085;
 
-app.get('*', function(req, res) {
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('*',function(req, res) {
 		const fullurl = req.hostname + req.originalUrl;
 		console.log(fullurl);
 
     res.sendFile( __dirname + '/index.html');
 });
-app.use(express.static(path.join(__dirname)));
 
 app.listen(adminPort, () => {
   console.log(`listening on ${adminPort}`);
