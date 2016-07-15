2a99e1b3f2ea586c87226418a711428fdcb90535
djtouchette <djtouchette1993@gmail.com>
2016-07-14 23:01:35 -0700

    Added file serving

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-var app = require('express')();
+const express = require('express');
+const app = express();
+const path = require('path');
 
 app.get('*', function(req, res) {
+		const fullurl = req.hostname + req.originalUrl;
+		console.log(fullurl);
 
-		var fullurl = req.hostname + req.originalUrl;
-			console.log(fullurl);
-
-      res.sendFile( __dirname + '/build/index.html');
-
-
+    res.sendFile( __dirname + '/build/index.html');
 });
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(62102, () => {
   console.log('listening 3000');
