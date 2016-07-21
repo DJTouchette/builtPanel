33d91a12dc9db83bdb3188c6be270470d4b44455
djtouchette <djtouchette1993@gmail.com>
2016-07-21 11:07:00 -0700

    Trailing comma

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const adminPort = 62102;
 // const adminPort = 8022;
 
 
-app.use(express.static(__dirname);
+app.use(express.static(__dirname));
 
 app.get('*',function(req, res) {
 		const fullurl = req.hostname + req.originalUrl;
