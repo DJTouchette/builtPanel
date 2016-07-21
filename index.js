724ab52b394703ed54ac3a3ccd9625d589b10fcd
djtouchette <djtouchette1993@gmail.com>
2016-07-21 11:10:02 -0700

    djsahda

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const adminPort = 62102;
 
 app.use(express.static(__dirname));
 
-app.get('/admin/',function(req, res) {
+app.get('*',function(req, res) {
 		const fullurl = req.hostname + req.originalUrl;
 		console.log(fullurl);
 
